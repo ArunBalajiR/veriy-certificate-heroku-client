b0afed1b6328ae550615d798c6c6b77440f48c7b
ArunBalajiR <arunbalaji25062k@gmail.com>
2022-02-18 19:14:00 +0530

    initial commit

---
+++ client/build/precache-manifest.ff6300094c06469bfea62481eedc2ad1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b2425f9c3b89b91422bde5146014d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a833da5c6bc352f3050b",
+    "url": "/static/css/2.f050fc0f.chunk.css"
+  },
+  {
+    "revision": "a833da5c6bc352f3050b",
+    "url": "/static/js/2.412ee2f6.chunk.js"
+  },
+  {
+    "revision": "90ca363861dedd0b84d0651983098801",
+    "url": "/static/js/2.412ee2f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "279ddb2c135a2ab9b857",
+    "url": "/static/js/3.34ab36e0.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/3.34ab36e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee2c1b0c68af8f0fc6ee",
+    "url": "/static/js/4.1af10fab.chunk.js"
+  },
+  {
+    "revision": "9127c945005b1c05fcb5ed1efd0e0e53",
+    "url": "/static/js/4.1af10fab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df31b45f1f4e3f00412b",
+    "url": "/static/js/5.9ba48838.chunk.js"
+  },
+  {
+    "revision": "4c2f580656954a1ad1c84a3bcb195d1c",
+    "url": "/static/js/5.9ba48838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "139b5f131d9838f30118",
+    "url": "/static/js/main.a8e3b68e.chunk.js"
+  },
+  {
+    "revision": "567c342f629b08eb3ecc",
+    "url": "/static/js/runtime-main.2d1c0df5.js"
+  },
+  {
+    "revision": "1b541dabbfaa4f471863eecdf98089dd",
+    "url": "/static/media/login-ANNA.1b541dab.svg"
+  },
+  {
+    "revision": "7fd5bfb88f8719c755c0f98cd6b5abb1",
+    "url": "/static/media/logo.7fd5bfb8.png"
+  },
+  {
+    "revision": "d663c4e36146fb9ecdb355e39785cd61",
+    "url": "/static/media/notfound.d663c4e3.png"
+  },
+  {
+    "revision": "9234483a312ef204ef1e089b630ada44",
+    "url": "/static/media/verifiedd.9234483a.png"
+  }
+]);
